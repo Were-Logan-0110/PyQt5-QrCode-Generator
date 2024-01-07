71164b1a70e6af3feb65767ff124eb77be835eb7
Ahmed Essam <123822233+Were-Logan-0110@users.noreply.github.com>
2024-01-07 05:14:19 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
-pip install -r .\requirements.txt
+python -m venv .
 ./Scripts/Activate.ps1
+pip install -r .\requirements.txt
 ./Scripts/nuitka --disable-console --standalone --lto=no --enable-plugin=pyqt5 main.py
